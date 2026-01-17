ec3755e4a855d8bd7dfa7c4ec6d20146eacaa1fc
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2026-01-17 13:09:53 +0300

    FIX

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,17 +26,21 @@ import { join } from 'path';
       ignoreEnvFile: false,
     }),
     MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
-        },
-      },
+  transport: {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // IMPORTANT
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS, // Gmail APP PASSWORD
+    },
+    requireTLS: true,
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 20000,
+  },
       defaults: {
-        from: '"Smatchin" <noreply@smatchin.com>',
+        from: '"Check mg" <noreply@checkmg.com>',
       },
       template: {
         dir: __dirname + '/templates',
